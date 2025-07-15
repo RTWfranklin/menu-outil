269bd7616f186e4e9cf58a6afd7fa7c3ca7744ca
RTWfranklin <gaspardrtw@gmail.com>
2025-07-15 23:15:06 +0200

    Mise à jour avec nouvelle version complète du site

--- js/ui.js
+++ js/ui.js
@@ -366,7 +366,8 @@ subItemsDiv.ondrop = function(e) {
           itemDragHandle.className = 'drag-handle';
           itemDragHandle.draggable = true;
           itemDragHandle.ondragstart = function(e) {
-            console.log('DEBUG drag handle', {thisCat, thisSubcat, itemIndex});
+            // Ajoute ce log :
+            console.log('DEBUG ids', thisCat.id, thisSubcat.id, itemIndex);
             const payload = JSON.stringify({
               fromCatId: thisCat.id,
               fromSubcatId: thisSubcat.id,
